7b6a84016513b015ef9ad23600d9601c11c4dc4a
Varun Goel <varun.varungoel.goel@gmail.com>
2022-02-17 07:44:11 +0530

    list in python

---
+++ list.py
@@ -0,0 +1,12 @@
+my_lst = list()
+my_lst = [6,3,1,8]
+print(my_lst)
+my_lst.pop(2)
+print(my_lst)
+my_lst.reverse()
+print(my_lst)
+10 in my_lst
+my_lst.remove(3)
+print(my_lst)
+my_lst.sort()
+print(my_lst)
